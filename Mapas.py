9173097e713ddc9b73341072b777ba6f5ea689e4
AnaPaula <anapaulagoetze@gmail.com>
2024-06-23 14:37:36 -0300

    apply: creating class mapas.py and starting main

---
+++ Mapas.py
@@ -0,0 +1,83 @@
+import folium
+from folium.plugins import Geocoder #AntPath
+import pandas as pd
+class Mapas:
+    def __init__(self, arquivo_excel='pontos_santacruz.xlsx'):
+        self.mapa = folium.Map(location=[-29.71886949612006, -52.42644538100079], zoom_start=15)
+        self.df = pd.read_excel(arquivo_excel)
+        self.df.dropna(subset=['LATITUDE', 'LONGITUDE'], inplace=True)
+        self.adicionar_marcadores()
+        self.adicionar_geocoder()
+
+    # Função para adicionar as "lojas"
+    def adicionar_marcadores(self):
+        for local, lat, lon, desc in zip(self.df.LOCAL, self.df.LATITUDE.values, self.df.LONGITUDE.values, self.df.DESCRICAO.values):
+            html = f"""
+            <strong>{local}</strong><br>
+            {desc}<br>
+            """
+            # Configuração do ícone do waypoint
+            folium.Marker(
+                [lat, lon],
+                popup=folium.Popup(html, max_width=250),
+                icon=folium.Icon(color='purple', icon='info-sign')
+            ).add_to(self.mapa)
+
+    # Essa função não está funcionando (JavaScript)
+    def adicionar_geocoder(self):
+      Geocoder(position='topleft').add_to(self.mapa)
+
+    # Lista todas as "lojas" presentes no arquivo xlsx
+    def listar_waypoints(self):
+      for index, row in self.df.iterrows():
+        local = row['LOCAL']
+        desc = row['DESCRICAO']
+        print(f"{local}: - {desc}")
+        print("-" * 80)
+
+    # Adiciona uma "loja" na planilha
+    def adicionar_local(self, local, lat, lon, desc):
+      new_row = pd.DataFrame({
+          'LOCAL': [local],
+          'LATITUDE': [lat],
+          'LONGITUDE': [lon],
+          'DESCRICAO': [desc]
+      })
+      updated_dataframe = pd.concat([self.df, new_row], ignore_index=True)
+
+      # Salva o dataframe atualizado de volta à planilha
+      updated_dataframe.to_excel('pontos_santacruz.xlsx', index=False)
+
+      self.df = pd.concat([self.df, new_row], ignore_index=True)
+      self.df.to_excel('pontos_santacruz.xlsx', index=False)
+      self.df = updated_dataframe
+      self.adicionar_marcadores()
+
+      return updated_dataframe
+
+    # Busca uma "loja" específica, passando o nome da "loja"
+    def buscar_waypoint(self, local):
+      waypoint = self.df[self.df['LOCAL'] == local]
+      if not waypoint.empty:
+        return {
+            'latitude': waypoint['LATITUDE'].values[0],
+            'longitude': waypoint['LONGITUDE'].values[0],
+            'descricao': waypoint['DESCRICAO'].values[0]
+        }
+      return None
+
+    # Remove uma "loja" da planilha
+    def remover_waypoint(self, local):
+      self.df = self.df[self.df['LOCAL'] != local]
+      self.df.to_excel('pontos_santacruz.xlsx', index = False)
+
+    # Atualiza a descrição (descontos) de uma "loja"
+    def atualizar_waypoint(self, local, novo_lat, novo_lon, nova_desc):
+      self.df.loc[self.df['LOCAL'] == local, ['DESCRICAO']] = [nova_desc]
+      self.df.to_excel('pontos_santacruz.xlsx', index = False)
+      self.df = pd.read_excel('pontos_santacruz.xlsx')
+
+    # Função para exibir o mapa com os waypoints (lojas)
+    def mostrar_mapa(self):
+      return self.mapa
+
---
+++ main.py
@@ -0,0 +1,72 @@
+
+from IPython.display import display #Javascript
+from Mapas import Mapas
+
+# Função para exibir o menu
+def menu():
+    print("\n╔═══════════[ Menu ]═══════════╗")
+    print("║(1) Exibir Mapa               ║")
+    print("║(2) Adicionar Local           ║")
+    print("║(3) Listar pontos registrados ║")
+    print("║(4) Buscar Waypoint           ║")
+    print("║(5) Atualizar Waypoint        ║")
+    print("║(9) {Remover Waypoint}        ║")
+    print("║(0) Sair                      ║")
+    print("╚══════════════════════════════╝")
+
+if __name__ == "__main__":
+  mapa = Mapas()
+
+  while True:
+    menu()
+    op = input("\n> ")
+
+    # Mostrar Mapa
+    if op == '1':
+      display(mapa.mostrar_mapa())
+
+    # Adicinar Local
+    elif op == '2':
+      local = input("Nome do local: ")
+      lat = float(input("Latitude: "))
+      lon = float(input("Longitude: "))
+      desc = input("Descrição: ")
+      mapa.adicionar_local(local, lat, lon, desc)
+      print("Local adicionado com sucesso!")
+
+    # Listar pontos registrados
+    elif op == '3':
+      mapa.listar_waypoints()
+
+    # Buscar Waypoint
+    elif op == '4':
+      local = input("Nome do local: ")
+      waypoint = mapa.buscar_waypoint(local)
+      if waypoint:
+        print(f"Local: {local}")
+        print(f"Latitude: {waypoint['latitude']}")
+        print(f"Longitude: {waypoint['longitude']}")
+        print(f"Descrição: {waypoint['descricao']}")
+      else:
+        print("Local não encontrado.")
+
+    # Atualizar Waypoint
+    elif op == '5':
+      local = input("Nome do local: ")
+      nova_desc = input("Nova descrição: ")
+      mapa.atualizar_waypoint(local, None, None, nova_desc)
+      print("Local atualizado com sucesso!")
+
+    # Remover Waypoint
+    elif op == '9':
+      local = input("Nome do local: ")
+      mapa.remover_waypoint(local)
+      print("Local removido com sucesso!")
+
+    # Sair
+    elif op == '0':
+      print("Saindo...")
+      break
+
+    else:
+      print("Opção inválida!")
